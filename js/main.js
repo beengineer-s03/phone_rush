e50c2f1edcdd743b5b5be0076f9021c0e62eaf40
beengineer-s03 <beengineer.umeda03.student@gmail.com>
2025-01-05 17:33:57 +0900

    h

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ let helper;
 let player;
 let playerBox;
 let player_v_y = 0;
-const initial_velocity = 0.8;
+const initial_velocity = 0.01;
 let isJumping = false;
 let isMoving = false;
 let box_X;
